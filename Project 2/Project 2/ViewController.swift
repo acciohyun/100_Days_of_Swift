30aaa5ad820e7add6aafa8667a83ca9f7a3146d6
Hyun Lee <hyunlee@10-141-55-126.0200.pos.kr.iosda.org>
2024-04-23 12:04:30 +0900

    Init: Project 2

---
+++ Project 2/Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by Hyun Lee on 4/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
