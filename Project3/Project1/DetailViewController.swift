2713eac9f2233ac4be1d801d8661cf37777891db
Hyun Lee <hyunlee@Hyuns-MacBook-Pro.local>
2024-04-26 20:18:26 +0900

    Feat: Add sharing feature using ActivityViewController. Feat: Allowing app to access photo library by adding on info.plist

--- Project3/Project1/DetailViewController.swift
+++ Project3/Project1/DetailViewController.swift
@@ -18,6 +18,7 @@ class DetailViewController: UIViewController {
         
         if let index{
             title = "Picture \(index + 1) of \(totalCount ?? 0)"
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         }
         navigationItem.largeTitleDisplayMode = .never
         if let imageToLoad = selectedImage{
@@ -34,6 +35,17 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = ImageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
 
     /*
