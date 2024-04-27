edfbbc7479f9c9e980aa15935d6e198fac765c67
Hyun Lee <hyunlee@Hyuns-MacBook-Pro.local>
2024-04-27 15:53:25 +0900

    Init: Milestone 1 Challenge project. Recap Project 1-3

--- Project3/Project1/DetailViewController.swift
+++ Project3/Project1/DetailViewController.swift
@@ -42,8 +42,13 @@ class DetailViewController: UIViewController {
             print("No image found")
             return
         }
+        
+        guard let name = selectedImage else{
+            print("Name not found")
+            return
+        }
 
-        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        let vc = UIActivityViewController(activityItems: [image, name], applicationActivities: [])
         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(vc, animated: true)
     }
