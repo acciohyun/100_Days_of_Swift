2e0edc9e6e8b3a9de4a4e6720cf708df23093be7
Hyun Lee <hyunlee@Hyuns-MacBook-Pro.local>
2024-05-11 11:56:54 +0900

    Init: Initialise Project 8

---
+++ Project 8/Project 8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 8
+//
+//  Created by Hyun Lee on 5/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
