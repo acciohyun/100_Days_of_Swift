29997af86c319422ad1a240dc5e366f0914490d4
Hyun Lee <hyunlee@10-141-55-126.0200.pos.kr.iosda.org>
2024-05-21 12:20:00 +0900

    Add fireparticles to when a ball is destroyed

--- Project 11/Project 11/GameScene.swift
+++ Project 11/Project 11/GameScene.swift
@@ -139,6 +139,11 @@ extension GameScene: SKPhysicsContactDelegate{
     }
     
     func destroy(ball: SKNode){
+        if let fireParticles = SKEmitterNode(fileNamed: "FireParticles"){
+            fireParticles.position = ball.position
+            addChild(fireParticles)
+        }
+        
         ball.removeFromParent()
     }
     
