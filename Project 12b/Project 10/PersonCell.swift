929e8c5756df750658531e89ab6f8fd3fc7f907e
Hyun Lee <hyunlee@10-141-55-126.0200.pos.kr.iosda.org>
2024-05-23 10:30:41 +0900

    Init: Create Project 12a and 12b

---
+++ Project 12b/Project 10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by Hyun Lee on 5/16/24.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
