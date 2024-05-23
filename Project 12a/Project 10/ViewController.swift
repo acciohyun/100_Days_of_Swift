af0a8a363b6eba2a92c5933ef7b63dc7e55769ed
Hyun Lee <hyunlee@10-141-55-126.0200.pos.kr.iosda.org>
2024-05-23 11:09:19 +0900

    Feat: Save Person data using NSCoding

--- Project 12a/Project 10/ViewController.swift
+++ Project 12a/Project 10/ViewController.swift
@@ -13,7 +13,15 @@ class ViewController: UICollectionViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))    }
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        
+        let defaults = UserDefaults.standard
+        if let savedPeople = defaults.object(forKey: "people") as? Data {
+            if let decodedPeople = try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(savedPeople) as? [Person]{
+                people = decodedPeople
+            }
+        }
+    }
 
 }
 
@@ -53,6 +61,7 @@ extension ViewController{
             person.name = newName
             
             self?.collectionView.reloadData()
+            self?.save()
         })
         
         present(ac, animated: true)
@@ -81,12 +90,21 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
         let person = Person(name: "Unknown", image: imageName)
         people.append(person)
         collectionView.reloadData()
-        
+        save()
         dismiss(animated: true)
     }
     
     func getDocumentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0]
+    }
+}
+
+extension ViewController{
+    func save(){
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: people, requiringSecureCoding: false){ // converts array people into a Data Object
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "people")
+        }
     }
 }
