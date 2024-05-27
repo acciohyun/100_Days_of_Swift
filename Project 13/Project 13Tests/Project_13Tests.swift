35a43e877f73d2fdb066f26513ce2533dbacd15e
Hyun Lee <hyunlee@Hyuns-MacBook-Pro.local>
2024-05-27 11:47:26 +0900

    Fix: Replace file wot UIKit file

--- Project 13/Project 13Tests/Project_13Tests.swift
+++ Project 13/Project 13Tests/Project_13Tests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import Project_13
 
 final class Project_13Tests: XCTestCase {
 
@@ -27,7 +28,7 @@ final class Project_13Tests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
