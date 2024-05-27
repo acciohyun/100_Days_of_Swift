a0ed1ddf7bc12593e6a1977dd4141e6954042487
Hyun Lee <hyunlee@Hyuns-MacBook-Pro.local>
2024-05-27 12:30:04 +0900

    Feat: Add ability to add photos

--- Project 13/Project 13/ViewController.swift
+++ Project 13/Project 13/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UIButton!
+    @IBAction func intensityChanged(_ sender: Any) {
+        
+    }
+    
+    var currentImage: UIImage!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
 
+    func changeFilter(){
+        
+    }
+    func save() {
+        
+    }
+    
+    @objc func importPicture(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
 
 }
 
+extension ViewController{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else {return}
+        dismiss(animated: true)
+        currentImage = image
+    }
+}
