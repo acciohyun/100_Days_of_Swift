c2820d9832c135220c53c48a0a42039fd53ee34b
Hyun Lee <hyunlee@Hyuns-MacBook-Pro.local>
2024-06-01 15:01:40 +0900

    Init: Initialise MVC Practice project https://www.hackingwithswift.com/articles/86/how-to-move-data-sources-and-delegates-out-of-your-view-controllers

---
+++ MVC Practice/MVC Practice/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  MVC Practice
+//
+//  Created by Hyun Lee on 6/1/24.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+
+}
+
